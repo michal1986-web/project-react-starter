f9a3640bc9e1df993a3479d37677d72997af2a80
Michał Grzegorczyk <michalgrzegorczyk4@gmail.com>
2020-01-19 17:16:36 +0100

    Add first React

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <main>
         <h1>My first React app</h1>
-      </div>
+        <h2>Hello world!</h2>
+      </main>
     )
   }
 }
 
+//defaul - Dzięki niemu, importując App w pliku index.js, możemy pominąć nawiasy klamrowe, czyli nie musimy pisać import {App} from '...';, tak jak do tej pory to robiliśmy
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
+//znajdź element, którego id to 'app', i wstaw do niego komponent App
 ReactDOM.render(<App />, document.getElementById('app'));
 
