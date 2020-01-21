9f1de95daa4eb5698dfc20ade74a51529e32b23a
Michał Grzegorczyk <michalgrzegorczyk4@gmail.com>
2020-01-21 18:57:33 +0100

    Add Card and Icon

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
 
@@ -18,6 +19,7 @@ class List extends React.Component {
     imageUrl: PropTypes.string, // string = tekst
     description: PropTypes.node,
     columns: PropTypes.array,
+    addColumn: PropTypes.func,
   }  
 
   static defaultProps = {
@@ -25,6 +27,22 @@ class List extends React.Component {
     description: settings.defaultListDescription,
   }
 
+  addColumn(title){
+    this.setState(state => (
+      {
+        columns: [
+          ...state.columns,
+          {
+            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
+  }
+  
   render() {
     return (
       <section className={styles.component}>
@@ -36,15 +54,19 @@ class List extends React.Component {
         </div>
 
         <div className={styles.columns}>
-
           {this.state.columns.map(({key, ...columnProps}) => (
-            <Column key={key} {...columnProps} />
+          <Column key={key} {...columnProps} />
           ))}
-          
         </div>
+
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+        </div>
+
 
       </section>
     )
+    // console.log('addColumn:', addColumn);
   }
 }
  
