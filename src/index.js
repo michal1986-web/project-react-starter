27c909ce23c92bba283a90c52e68226a112031fe
Michal <michalgrzegorczyk4@gmail.com>
2020-01-22 14:59:52 +0100

    Test 06

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
- 
+
 //znajdź element, którego id to 'app', i wstaw do niego komponent App
 ReactDOM.render(<App />, document.getElementById('app'));
 
