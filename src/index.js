d1f34105ea9f7d22ca72d8ef833acfdb809c735f
Misiek <michalgrzegorczyk4@gmail.com>
2020-01-24 22:48:58 +0100

    Test 08

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
+Test 1 ' 1 '
 
 //znajdź element, którego id to 'app', i wstaw do niego komponent App
 ReactDOM.render(<App />, document.getElementById('app'));
