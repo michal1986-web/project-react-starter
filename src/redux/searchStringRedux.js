3b0abea04d7030fc0b9e242edcafc611f38bdcfa
Misiek <michalgrzegorczyk4@gmail.com>
2020-01-25 20:26:23 +0100

    Add Working Search

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -6,14 +6,23 @@ export const countVisibleCards = ({cards, searchString}) => cards.filter(card =>
 export const countAllCards = ({cards}) => cards.length;
 
 // action name creator
+// const reducerName = 'cards';
+const reducerName = 'search';
+
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+// export const ADD_CARD = createActionName('ADD_CARD');
+export const CHANGE = createActionName('CHANGE');
 
 // action creators
+export const createActionChangeSearchString = payload => ({payload, type: CHANGE});
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE:
+      return action.payload;
     default:
       return statePart;
   }
