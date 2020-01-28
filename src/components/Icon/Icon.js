3b8c7819b2e42a6604737fb49df4bafc85af87b4
Misiek <michalgrzegorczyk4@gmail.com>
2020-01-28 13:02:00 +0100

    Add icons and flex

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -3,6 +3,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // eslint-disable-next-line no-unused-vars
-const Icon = props => (<i className={'fas fa-${props.name}'}></i>);
+const Icon = props => (<i className={`fas fa-${props.name}`}></i>);
+
+Icon.propTypes = {
+  name: PropTypes.string,
+};
 
 export default Icon;
